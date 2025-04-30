312ce95d74acba294db1b14b3fdc193b6853d337
ABDINDUSTRY <sabdoulhakim@yahoo.fr>
2025-05-01 01:34:01 +0200

    Update stop.sh

--- scripts /stop.sh
+++ scripts /stop.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Arrêt du programme"
-docker compose down
+
+echo " Arrêt de l'infrastructure OPSCI..."
+
+# Arrêter tous les docker-compose par dossier
+docker-compose -f strapi/docker-compose.yml down
+docker-compose -f opsci-strapi-frontend-master/docker-compose.yml down
+docker-compose -f kafka_producer/docker-compose.yml down
+docker-compose -f mqtt/docker-compose.yml down
+docker-compose -f mqtt-kafka-connector/docker-compose.yml down
+
+echo " Tous les services sont arrêtés."
